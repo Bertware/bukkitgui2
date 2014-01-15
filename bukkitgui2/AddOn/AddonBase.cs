e51533c68ad59ca3123297f2ae33585c70ea4fe0
Bertware <contact@bertware.net>
2014-01-15 13:39:06 +0100

    Added more items to file structure

---
+++ bukkitgui2/AddOn/AddonBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bukkitgui2.AddOn
+{
+	class AddonBase
+	{
+		public void Initiate()
+		{
+
+		}
+
+	}
+}
