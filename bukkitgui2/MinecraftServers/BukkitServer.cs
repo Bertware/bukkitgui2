a0fcfd1e28547b04fcb7e231b5d8600faeac48f3
Bertware <contact@bertware.net>
2014-01-15 11:39:01 +0100

    Initial commit

---
+++ bukkitgui2/MinecraftServers/BukkitServer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bukkitgui2.MinecraftServers
+{
+	class BukkitServer
+	{
+	}
+}
---
+++ bukkitgui2/MinecraftServers/MinecraftServerLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bukkitgui2.MinecraftServers
+{
+	class MinecraftServerLoader
+	{
+
+
+	}
+
+}
