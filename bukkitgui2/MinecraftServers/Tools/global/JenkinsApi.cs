f5585f9905de03dec22feb21714b75e5b0522973
Bertware <bert.marcelis@outlook.com>
2014-02-20 14:40:14 +0100

    Added server tools file structure

---
+++ bukkitgui2/MinecraftServers/Tools/global/JenkinsApi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bukkitgui2.MinecraftServers.Tools.global
+{
+    /// <summary>
+    /// Allows to get latest build numbers and download links from jenkins repositories
+    /// </summary>
+    class JenkinsApi
+    {
+    }
+}
