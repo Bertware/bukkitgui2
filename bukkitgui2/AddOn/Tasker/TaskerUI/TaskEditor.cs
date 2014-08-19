d3f0d5b1170efb861a617a3b676fbb1d98986f46
Bertware <bert.marcelis@outlook.com>
2014-08-19 15:57:48 +0200

    Fixed missing files on github

---
+++ bukkitgui2/AddOn/Tasker/TaskerUI/TaskEditor.cs
@@ -0,0 +1,167 @@
+﻿// TaskEditor.cs in bukkitgui2/bukkitgui2
+// Created 2014/06/24
+// Last edited at 2014/08/17 11:19
+// ©Bertware, visit http://bertware.net
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Net.Bertware.Bukkitgui2.AddOn.Tasker.Action;
+using Net.Bertware.Bukkitgui2.AddOn.Tasker.Trigger;
+
+namespace Net.Bertware.Bukkitgui2.AddOn.Tasker.TaskerUI
+{
+	public partial class TaskEditor : Form
+	{
+		private readonly Task _taskToEdit;
+
+		private ITrigger _selectedTrigger;
+
+		public TaskEditor()
+		{
+			InitializeComponent();
+			PopulateUi();
+		}
+
+		public TaskEditor(Task taskToEdit)
+		{
+			_taskToEdit = taskToEdit;
+			InitializeComponent();
+			PopulateUi();
+		}
+
+		/// <summary>
+		///     Load Triggers
+		/// </summary>
+		private void PopulateUi()
+		{
+			cbTrigger.Items.Clear();
+			foreach (ITrigger trigger in Task.AllTriggers)
+			{
+				cbTrigger.Items.Add(trigger.Name);
+				if (_taskToEdit != null && _taskToEdit.Trigger.Name.Equals(trigger.Name))
+					cbTrigger.SelectedIndex = cbTrigger.Items.Count - 1; //select last
+			}
+
+			if (_taskToEdit == null) return;
+
+			txtName.Text = _taskToEdit.Name;
+			chkEnable.Checked = _taskToEdit.Enabled;
+			txtTriggerParam.Text = _taskToEdit.Trigger.Parameters;
+			foreach (IAction action in _taskToEdit.Actions)
+			{
+				AddActionControl();
+				ActionSelector selector = ((ActionSelector) gbAction.Controls[gbAction.Controls.Count - 1]);
+				selector.SetAction(action);
+			}
+		}
+
+		/// <summary>
+		///     Trigger changed
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void cbTrigger_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			foreach (ITrigger trigger in Task.AllTriggers)
+			{
+				if (cbTrigger.SelectedItem.ToString().Equals(trigger.Name)) _selectedTrigger = trigger;
+			}
+			if (_selectedTrigger != null)
+			{
+				lblTriggerDescription.Text = _selectedTrigger.ParameterDescription;
+			}
+		}
+
+		/// <summary>
+		///     Validate trigger
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void txtTriggerParam_TextChanged(object sender, EventArgs e)
+		{
+			if (_selectedTrigger == null) return;
+
+			if (_selectedTrigger.ValidateInput(txtTriggerParam.Text))
+			{
+				errorProvider.SetError(txtTriggerParam, "");
+			}
+			else
+			{
+				errorProvider.SetError(txtTriggerParam,
+					"Invalid parameter!" + Environment.NewLine + _selectedTrigger.ParameterDescription);
+			}
+		}
+
+		/// <summary>
+		///     Handle button click for adding a new action
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnNewaction_Click(object sender, EventArgs e)
+		{
+			AddActionControl();
+		}
+
+		/// <summary>
+		///     Add an action control
+		/// </summary>
+		private void AddActionControl()
+		{
+			Height += 180;
+			ActionSelector selector = new ActionSelector(gbAction.Controls.Count)
+			{
+				Location = new Point(6, 50 + 180*(gbAction.Controls.Count - 1))
+			};
+			gbAction.Controls.Add(selector);
+			if (gbAction.Controls.Count > 4) btnNewAction.Enabled = false;
+		}
+
+		/// <summary>
+		///     Save task
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			Task t = ParseFields();
+			if (_taskToEdit == null)
+			{
+				Tasker.Reference.AddTask(t);
+			}
+			else
+			{
+				Tasker.Reference.SaveTask(_taskToEdit, t);
+			}
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+
+		/// <summary>
+		///     Cancel edit
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+			Close();
+		}
+
+		private Task ParseFields()
+		{
+			ITrigger trigger = _selectedTrigger;
+			trigger.Parameters = txtTriggerParam.Text;
+			List<IAction> actions = new List<IAction>();
+
+			foreach (Control control in gbAction.Controls)
+			{
+				if (!(control is ActionSelector)) continue;
+				ActionSelector selector = (ActionSelector) control;
+				actions.Add(selector.GetAction());
+			}
+			return new Task(txtName.Text, chkEnable.Checked, trigger, actions);
+		}
+	}
+}
---
+++ bukkitgui2/AddOn/Updater/UpdaterSettings.Designer.cs
@@ -0,0 +1,142 @@
+﻿using MetroFramework.Controls;
+
+namespace Net.Bertware.Bukkitgui2.AddOn.Updater
+{
+	partial class UpdaterSettings
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.GBOptionsInfoAbout = new System.Windows.Forms.GroupBox();
+			this.BtnInfoAppUpdater = new MetroFramework.Controls.MetroButton();
+			this.lblInfoAppLatest = new MetroFramework.Controls.MetroLabel();
+			this.lblInfoAppCopyright = new MetroFramework.Controls.MetroLabel();
+			this.lblInfoAppVersion = new MetroFramework.Controls.MetroLabel();
+			this.lblInfoAppAuthors = new MetroFramework.Controls.MetroLabel();
+			this.lblInfoAppName = new MetroFramework.Controls.MetroLabel();
+			this.GBOptionsInfoAbout.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// GBOptionsInfoAbout
+			// 
+			this.GBOptionsInfoAbout.Controls.Add(this.BtnInfoAppUpdater);
+			this.GBOptionsInfoAbout.Controls.Add(this.lblInfoAppLatest);
+			this.GBOptionsInfoAbout.Controls.Add(this.lblInfoAppCopyright);
+			this.GBOptionsInfoAbout.Controls.Add(this.lblInfoAppVersion);
+			this.GBOptionsInfoAbout.Controls.Add(this.lblInfoAppAuthors);
+			this.GBOptionsInfoAbout.Controls.Add(this.lblInfoAppName);
+			this.GBOptionsInfoAbout.Location = new System.Drawing.Point(3, 3);
+			this.GBOptionsInfoAbout.Name = "GBOptionsInfoAbout";
+			this.GBOptionsInfoAbout.Size = new System.Drawing.Size(554, 176);
+			this.GBOptionsInfoAbout.TabIndex = 1;
+			this.GBOptionsInfoAbout.TabStop = false;
+			this.GBOptionsInfoAbout.Text = "About";
+			// 
+			// BtnInfoAppUpdater
+			// 
+			this.BtnInfoAppUpdater.Location = new System.Drawing.Point(6, 147);
+			this.BtnInfoAppUpdater.Name = "BtnInfoAppUpdater";
+			this.BtnInfoAppUpdater.Size = new System.Drawing.Size(542, 23);
+			this.BtnInfoAppUpdater.TabIndex = 5;
+			this.BtnInfoAppUpdater.Text = "Open Updater";
+			this.BtnInfoAppUpdater.UseSelectable = true;
+			this.BtnInfoAppUpdater.Click += new System.EventHandler(this.BtnInfoAppUpdater_Click);
+			// 
+			// lblInfoAppLatest
+			// 
+			this.lblInfoAppLatest.AutoSize = true;
+			this.lblInfoAppLatest.Location = new System.Drawing.Point(6, 125);
+			this.lblInfoAppLatest.Name = "lblInfoAppLatest";
+			this.lblInfoAppLatest.Size = new System.Drawing.Size(90, 19);
+			this.lblInfoAppLatest.TabIndex = 4;
+			this.lblInfoAppLatest.Text = "Latest version:";
+			// 
+			// lblInfoAppCopyright
+			// 
+			this.lblInfoAppCopyright.AutoSize = true;
+			this.lblInfoAppCopyright.Location = new System.Drawing.Point(6, 80);
+			this.lblInfoAppCopyright.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+			this.lblInfoAppCopyright.Name = "lblInfoAppCopyright";
+			this.lblInfoAppCopyright.Size = new System.Drawing.Size(70, 19);
+			this.lblInfoAppCopyright.TabIndex = 3;
+			this.lblInfoAppCopyright.Text = "Copyright:";
+			// 
+			// lblInfoAppVersion
+			// 
+			this.lblInfoAppVersion.AutoSize = true;
+			this.lblInfoAppVersion.Location = new System.Drawing.Point(6, 59);
+			this.lblInfoAppVersion.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+			this.lblInfoAppVersion.Name = "lblInfoAppVersion";
+			this.lblInfoAppVersion.Size = new System.Drawing.Size(55, 19);
+			this.lblInfoAppVersion.TabIndex = 2;
+			this.lblInfoAppVersion.Text = "Version:";
+			// 
+			// lblInfoAppAuthors
+			// 
+			this.lblInfoAppAuthors.AutoSize = true;
+			this.lblInfoAppAuthors.Location = new System.Drawing.Point(6, 38);
+			this.lblInfoAppAuthors.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+			this.lblInfoAppAuthors.Name = "lblInfoAppAuthors";
+			this.lblInfoAppAuthors.Size = new System.Drawing.Size(57, 19);
+			this.lblInfoAppAuthors.TabIndex = 1;
+			this.lblInfoAppAuthors.Text = "Authors:";
+			// 
+			// lblInfoAppName
+			// 
+			this.lblInfoAppName.AutoSize = true;
+			this.lblInfoAppName.Location = new System.Drawing.Point(6, 17);
+			this.lblInfoAppName.Margin = new System.Windows.Forms.Padding(3, 1, 3, 1);
+			this.lblInfoAppName.Name = "lblInfoAppName";
+			this.lblInfoAppName.Size = new System.Drawing.Size(48, 19);
+			this.lblInfoAppName.TabIndex = 0;
+			this.lblInfoAppName.Text = "Name:";
+			// 
+			// UpdaterSettings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.Controls.Add(this.GBOptionsInfoAbout);
+			this.Name = "UpdaterSettings";
+			this.Size = new System.Drawing.Size(560, 490);
+			this.Load += new System.EventHandler(this.UpdaterSettings_Load);
+			this.GBOptionsInfoAbout.ResumeLayout(false);
+			this.GBOptionsInfoAbout.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		internal System.Windows.Forms.GroupBox GBOptionsInfoAbout;
+		internal MetroButton BtnInfoAppUpdater;
+		internal MetroLabel lblInfoAppLatest;
+		internal MetroLabel lblInfoAppCopyright;
+		internal MetroLabel lblInfoAppVersion;
+		internal MetroLabel lblInfoAppAuthors;
+		internal MetroLabel lblInfoAppName;
+	}
+}
