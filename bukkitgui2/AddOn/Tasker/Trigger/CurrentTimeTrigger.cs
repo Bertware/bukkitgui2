b80c325c4863a9201724229c842f8f8513911de4
Bertware <bert.marcelis@outlook.com>
2015-01-25 19:41:57 +0100

    Fixed current time trigger

--- bukkitgui2/AddOn/Tasker/Trigger/CurrentTimeTrigger.cs
+++ bukkitgui2/AddOn/Tasker/Trigger/CurrentTimeTrigger.cs
@@ -55,13 +55,13 @@ namespace Net.Bertware.Bukkitgui2.AddOn.Tasker.Trigger
 
         public bool Enabled { get; protected set; }
 
-		/// <summary>
-		///     Timer interval in seconds
-		/// </summary>
-		private const int TimerInterval = 5;
+        /// <summary>
+        ///     Timer interval in seconds
+        /// </summary>
+        private const int TimerInterval = 5;
 
-		private Timer _timerCheckCurrentTime;
-		private TimeSpan[] _times;
+        private Timer _timerCheckCurrentTime;
+        private TimeSpan[] _times;
 
         public void Enable()
         {
@@ -76,7 +76,7 @@ namespace Net.Bertware.Bukkitgui2.AddOn.Tasker.Trigger
 
             _timerCheckCurrentTime = new Timer(TimerInterval*1000);
             _timerCheckCurrentTime.Elapsed += check_time;
-			_timerCheckCurrentTime.Start();
+            _timerCheckCurrentTime.Start();
             Enabled = true;
         }
 
@@ -92,7 +92,6 @@ namespace Net.Bertware.Bukkitgui2.AddOn.Tasker.Trigger
             Enabled = false;
         }
 
-      
 
         /// <summary>
         ///     Check the current time, and see if the trigger should go off.
@@ -101,9 +100,9 @@ namespace Net.Bertware.Bukkitgui2.AddOn.Tasker.Trigger
         {
             foreach (TimeSpan time in _times)
             {
-	            int dt = Math.Abs(DateTime.Now.TimeOfDay.Subtract(time).Seconds) ;
-				if (dt < (TimerInterval/2))
-                    TaskerTriggerFired.Invoke();
+                double dt = Math.Abs(DateTime.Now.TimeOfDay.Subtract(time).TotalSeconds);
+                if (!(dt < (TimerInterval/2))) continue;
+                if (TaskerTriggerFired != null) TaskerTriggerFired.Invoke();
             }
         }
 
